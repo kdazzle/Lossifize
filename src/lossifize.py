401c6624d679102e95d77e5ff590dc1591189450
Kyle Valade <kylevalade@gmail.com>
2012-01-24 23:42:46 -0800

    Added a directory argument to the command line

--- src/lossifize.py
+++ src/lossifize.py
@@ -14,12 +14,13 @@ def main(argv=None):
     inputFile = None
     help = None
     copies = None
+    directory = None
 
-    argList = {'file': None, 'copies': None}
+    argList = {'file': inputFile, 'copies': copies, 'directory': directory}
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "fc:h", ["file=", "help=",
-        "copies="])
+        opts, args = getopt.getopt(sys.argv[1:], "fct:h", ["file=", "help=",
+        "copies=", "targetDirectory"])
         for opt, arg in opts:
             if opt in ("-h", "--help"):
                 help = getHelpText()
@@ -30,6 +31,8 @@ def main(argv=None):
             elif opt in ("-c", "--copies"):
                 argList['copies'] = int(arg)
                 print "Copies: ", copies
+            elif opt in ("-t", "--targetDirectory"):
+                argList['directory'] = arg
             else:
                 print "Incorrect arguments, -h for help."
 
@@ -41,12 +44,25 @@ def main(argv=None):
     if not argList['copies'] and not help:
         print "How many copies do you want?"
     if argList['file'] and argList['copies']:
-        outputDir = "/home/k-dazzle/Documents/Programming/Lossifize/copies"
-        lossifizeShit(argList, outputDir)
+        lossifize(argList, outputDir)
 
-def lossifizeShit(argList, outputDir):
+def getHelpText():
+    help ="""
+    --copies            -c  (required)  the amount of copies that you want to make - at
+                            least 30,000 is recommended
+    --file              -f  (required) the file that you are trying to copy
+    --help              -h  help
+    --targetDirectory   -t  (required) the directory you are saving to
+
+    Files will be saved into the copies folder in the Lossifize directory
+    """
+
+    return help
+
+def lossifize(argList, targetDir):
     inputFile = argList['file']
     copies = argList['copies']
+    targetDir = argList['dir']
 
     originalImage = Image.open(inputFile)
 
@@ -56,12 +72,12 @@ def lossifizeShit(argList, outputDir):
 
     resizedImage = originalImage.copy()
     resizedImage = resizedImage.resize((newWidth, newHeight))
-    resizedImage.save("%s/%s" % (outputDir, "resized.jpg"))
+    resizedImage.save("%s/%s" % (targetDir, "resized.jpg"))
 
 
     comparableImage = resizedImage.copy()
     comparableImage = comparableImage.resize(size)
-    comparableImage.save("%s/%s" % (outputDir, "compareImage.jpg"))
+    comparableImage.save("%s/%s" % (targetDir, "compareImage.jpg"))
 
     toCopy = resizedImage.copy()
     count = copies
@@ -70,7 +86,7 @@ def lossifizeShit(argList, outputDir):
         if i > 0:
             toCopy = Image.open(newLocation)
 
-        newLocation = "%s/%s.jpg" % (outputDir, i)
+        newLocation = "%s/%s.jpg" % (targetDir, i)
         toCopy = alterImage(toCopy)
         toCopy.save("%s" % (newLocation))
 
@@ -79,24 +95,12 @@ def lossifizeShit(argList, outputDir):
         if (i == 0 or (i % 100 == 0)):
             pass
         else:
-            oldLocation = "%s/%s.jpg" % (outputDir, i-1)
+            oldLocation = "%s/%s.jpg" % (targetDir, i-1)
             os.remove(oldLocation)
 
     finalImage = Image.open(newLocation)
     finalImage = finalImage.resize(size)
-    finalImage.save("%s/%s-%s" % (outputDir, count, "finalImage.jpg"))
-
-def getHelpText():
-    help ="""
-    --copies -c (required)  the amount of copies that you want to make - at
-                least 30,000 is recommended
-    --file -f (required) the file that you are trying to copy
-    --help -h  help
-
-    Files will be saved into the copies folder in the Lossifize directory
-    """
-
-    return help
+    finalImage.save("%s/%s-%s" % (targetDir, count, "finalImage.jpg"))
 
 def alterImage(image):
     x = randint(0,(image.size[0]-1))
